a46807d0cf02856efb615c7b9a18e3cb23a98558
BenjaJara98 <benjamin.jara1701@alumnos.ubiobio.cl>
2023-10-10 14:53:45 -0300

    arreglos esteticos

--- tdw/src/pages/Home.tsx
+++ tdw/src/pages/Home.tsx
@@ -1,12 +1,32 @@
 import Box from '@mui/material/Box';
+import Grid from '@mui/system/Unstable_Grid/Grid';
 import Card from "./components/Card"
 export default function Home() {
+
+
+    
     return (
-        <>
-            <Box sx={{ width: '100%' }}>
+         <>
+         <Grid container spacing={1}>
+           <Grid item md={4} xs={6}>
+           <Box sx={{ width: '100%' }}>
                 <Card></Card>
             </Box>
-        </>
+           </Grid>
+   
+           <Grid item md={4} xs={6}>
+           <Box sx={{ width: '100%' }}>
+                <Card></Card>
+            </Box>
+           </Grid>
+   
+           <Grid item md={4} xs={6}>
+           <Box sx={{ width: '100%' }}>
+                <Card></Card>
+            </Box>
+           </Grid>
+         </Grid>
+       </>
     )
 }
 
